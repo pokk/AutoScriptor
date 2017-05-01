704bc03d277f6aacda61afa4f253b47e957df319
Jieyi <pokkbaby@gmail.com>
2017-05-01 19:04:40 +0900

    Modified the zip utils can zip a file.

--- MagicBack/decorator_back_process.py
+++ MagicBack/decorator_back_process.py
@@ -4,6 +4,10 @@ from shutil import rmtree, copyfile, copytree
 
 from __init__ import warning_str
 
+from MagicBack.utils_zip import zip_files
+
+temp_folder = '~/Documents'
+
 
 class DecoratorCheckDestination:
     @staticmethod
@@ -34,25 +38,30 @@ class DecoratorCheckDestination:
                     os.mkdir(sync_folder)
 
                 src, dst = func(args[0], args[1], sync_folder, args[2])
+                
+                print(src)
 
-                # If there exists a src preference, then remove it.
-                for s, d in zip(src, dst):
-                    # Ignore the src which isn't exist.
-                    if not os.path.exists(s):
-                        msg_callback(warning_str % s.split('/')[-1])
-                        continue
-                    if os.path.exists(d):
-                        self.remove_backup(d)
-                    # For that there are only file.
-                    target_folder = '/'.join(d.split('/')[:-1])
-                    if not os.path.exists(target_folder):
-                        # If not, create it.
-                        os.mkdir(target_folder)
+                zip_files(src, os.path.join(temp_folder, '.'.join([args[1].split('.')[0], 'zip'])))
+                # print(dst)
 
-                    # Sync the preference.
-                    self.copy_backup(s, d)
-
-                    msg_callback(f'Finished sync {d.split("/")[-1]}\n')
+                # # If there exists a src preference, then remove it.
+                # for s, d in zip(src, dst):
+                #     # Ignore the src which isn't exist.
+                #     if not os.path.exists(s):
+                #         msg_callback(warning_str % s.split('/')[-1])
+                #         continue
+                #     if os.path.exists(d):
+                #         self.remove_backup(d)
+                #     # For that there are only file.
+                #     target_folder = '/'.join(d.split('/')[:-1])
+                #     if not os.path.exists(target_folder):
+                #         # If not, create it.
+                #         os.mkdir(target_folder)
+                # 
+                #     # Sync the preference.
+                #     self.copy_backup(s, d)
+                # 
+                #     msg_callback(f'Finished sync {d.split("/")[-1]}\n')
             else:
                 msg_callback(warning_str % 'Dropbox')
 
--- MagicBack/utils_zip.py
+++ MagicBack/utils_zip.py
@@ -4,11 +4,14 @@ import zipfile
 
 
 def zip_files(src_path, dst_path_with_filename='zipfile.zip'):
-    with zipfile.ZipFile(dst_path_with_filename, 'w', zipfile.ZIP_DEFLATED) as zf:
+    with zipfile.ZipFile(os.path.expanduser(dst_path_with_filename), 'w', zipfile.ZIP_DEFLATED) as zf:
         for path in src_path:
-            for root, dirs, files in os.walk(os.path.expanduser(path)):
-                for file in files:
-                    zf.write(os.path.join(root, file))
+            if os.path.isdir(path):
+                for root, dirs, files in os.walk(os.path.expanduser(path)):
+                    for file in files:
+                        zf.write(os.path.join(root, file))
+            elif os.path.isfile(path):
+                zf.write(path)
 
 
 def unzip_files(zipfile_path, dst_path):
@@ -17,8 +20,8 @@ def unzip_files(zipfile_path, dst_path):
 
 
 def main():
-    zip_files(['~/Downloads/aa', '~/Downloads/bb', '~/Workspace/note'], os.path.expanduser('~/Downloads/cool.zip'))
-    unzip_files('~/Downloads/cool.zip', '/')
+    zip_files(['/Users/jieyi/Library/Preferences/com.apple.Terminal.plist'], os.path.expanduser('~/Documents/test.zip'))
+    unzip_files('~/Downloads/terminal.zip', '~/Downloads')
 
 
 if __name__ == '__main__':
